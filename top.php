0a370feb8a36dda0656a5fd9884d52ce3169a4ba
keisuke <ke_sukesakuma@keisukenoMacBook-Pro.local>
2021-12-22 17:25:58 +0900

    トップ画面の作成

---
+++ top.php
@@ -0,0 +1,15 @@
+<?php
+   //一覧画面、サインアップ、ログインのリンクを作成する
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>todo App</title>
+</head>
+<body>
+    <a href="signup.php">サインアップ</a>
+    <a href="login.php">ログイン</a>
+    <a href="index.php">投稿画面</a>
+</body>
+</html>
